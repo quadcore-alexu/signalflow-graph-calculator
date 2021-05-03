a26feb86f6cfc0e89d80c9e1e494f84487cc62cd
Ahmad Waleed <a.waleedothman@gmail.com>
2021-05-03 22:55:56 +0200

    can get loops. also, added more complex test with good code coverage

--- src/main/java/model/tests/NodeVisitorTest.java
+++ src/main/java/model/tests/NodeVisitorTest.java
@@ -2,28 +2,25 @@ package model.tests;
 
 import interfaces.INode;
 import interfaces.ISignalFlowGraph;
-import model.Edge;
-import model.Node;
-import model.Path;
-import model.SignalFlowGraph;
-import org.junit.jupiter.api.BeforeAll;
+import model.*;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class NodeVisitorTest {
 
-    static ISignalFlowGraph graph = new SignalFlowGraph();
+    @Test
+    void simpleTest() {
+        ISignalFlowGraph graph = new SignalFlowGraph();
 
-    @BeforeAll
-    static void setUp() {
         INode start = new Node();
-        graph.setStart(start);
         INode second = new Node();
         INode third = new Node();
         INode fourth = new Node();
+
+        graph.setStart(start);
         graph.setEnd(fourth);
 
         new Edge(start, second, 5);
@@ -33,10 +30,7 @@ class NodeVisitorTest {
         new Edge(fourth, second, 8); //loop
         new Edge(fourth, fourth, 8); //self loop
         new Edge(fourth, third, 8); //loop
-    }
 
-    @Test
-    void visit() {
 
         graph.update();
         List<Path> paths = graph.getPaths();
@@ -49,5 +43,50 @@ class NodeVisitorTest {
         assertEquals(240 + 50, sumOfGain);
     }
 
-    //todo add tests from sheet
+    /*
+    question 3 in control, sheet4
+     */
+    @Test
+    void test1() {
+        ISignalFlowGraph graph = new SignalFlowGraph();
+
+        INode y1 = new Node();
+        INode y2 = new Node();
+        INode y3 = new Node();
+        INode y4 = new Node();
+        INode y5 = new Node();
+        INode y6 = new Node();
+
+        graph.setStart(y1);
+        graph.setEnd(y5);
+
+        new Edge(y1, y2, 1);
+        new Edge(y2, y3, 5);
+        new Edge(y3, y4, 10);
+        new Edge(y4, y5, 2);
+        new Edge(y5, y2, -1);
+        new Edge(y5, y4, -2);
+        new Edge(y4, y3, -1);
+        new Edge(y2, y6, 10);
+        new Edge(y6, y5, 2);
+        new Edge(y6, y6, -1);
+
+        graph.update();
+
+        List<Path> paths = graph.getPaths();
+        List<Loop> loops = graph.getLoops();
+
+        double sumOfPaths = 0;
+        for (Path p : paths) {
+            sumOfPaths += p.getGain();
+        }
+        assertEquals(120, sumOfPaths);
+
+        double sumOfLoops = 0;
+        for (Loop l : loops) {
+            sumOfLoops += l.getGain();
+        }
+        assertEquals(-135, sumOfLoops);
+    }
+
 }
